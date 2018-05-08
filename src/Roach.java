b7c2768cc7f5c27905cc4e7eced637e1ca3b50a2
Fredrik Svahn <fredriksvahn97@gmail.com>
2018-05-08 15:50:47 +0200

    add roaches

--- src/Roach.java
+++ src/Roach.java
@@ -8,6 +8,7 @@ public class Roach extends Enemy {
 
     @Override
     public void display(Graphics graphics, int x, int y) {
-
+        graphics.setColor(Color.RED);
+        graphics.fillOval(x - 5, y - 5, 10, 10);
     }
 }
