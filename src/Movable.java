8d2314962e746f5cf172b0942993a421e164f0be
frsvahn <frsvahn@kth.se>
2018-05-08 23:30:42 +0200

    store direction you last moved to

--- src/Movable.java
+++ src/Movable.java
@@ -6,6 +6,7 @@ public abstract class Movable {
     protected int x;
     protected int y;
     protected boolean solid = true;
+    protected Direction direction = Direction.RIGHT;
 
     public Movable(int x, int y) {
         this.x = x;
@@ -31,12 +32,14 @@ public abstract class Movable {
             case DOWNRIGHT: newX += 1; newY -= 1; break;
         }
 
-        return tryMoveTo(newX, newY, map);
+        if(tryMoveTo(newX, newY, map)) {
+            this.direction = direction;
+            return true;
+        }
+        return false;
     }
 
     public boolean tryMoveTo(int x, int y, Map map) {
-        System.out.println(tileFree(x, y, map));
-        System.out.println(solid);
         if(tileFree(x, y, map) || !solid) {
             map.getTile(this.x, this.y).removeMovable();
             this.x = x;
